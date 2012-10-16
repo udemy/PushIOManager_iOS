9a1fcd57c05fb2d32d8ac183c4d10ddabcf7ad4c
Dan Burcaw <burcaw@doubleencore.com>
2012-10-16 15:52:50 -0600

    add sample app – push io news

---
+++ PushIONewsSampleApp/newsTests/newsTests.h
@@ -0,0 +1,13 @@
+//
+//  newsTests.h
+//  newsTests
+//
+//  Created by Dan Burcaw on 7/26/12.
+//  Copyright (c) 2012 Push IO LLC. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface newsTests : SenTestCase
+
+@end
