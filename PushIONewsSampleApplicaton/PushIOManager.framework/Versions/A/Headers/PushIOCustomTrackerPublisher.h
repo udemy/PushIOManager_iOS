e14f26ad90602842c286897156674234fed33ff5
pushiodeploy <pushiollc@gmail.com>
2013-04-10 12:26:20 -0600

    Updated PushIOManager framework in sample project.

--- PushIONewsSampleApplicaton/PushIOManager.framework/Versions/A/Headers/PushIOCustomTrackerPublisher.h
+++ PushIONewsSampleApplicaton/PushIOManager.framework/Versions/A/Headers/PushIOCustomTrackerPublisher.h
@@ -2,7 +2,8 @@
 //  PushIOCustomTrackerPublisher.h
 //  PushIOManager
 //
-//  Copyright (c) 2013 Push IO LLC. All rights reserved.
+//  Created by Kendall Helmstetter Gelner on 9/19/12.
+//  Copyright (c) 2012 Push IO LLC. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
--- PushIONewsSampleApplicaton/PushIOManager.framework/Versions/A/Headers/PushIOTracker+PushIOCustomTracker.h
+++ PushIONewsSampleApplicaton/PushIOManager.framework/Versions/A/Headers/PushIOTracker+PushIOCustomTracker.h
@@ -2,7 +2,8 @@
 //  PushIOCustomTracker.h
 //  PushIOManager
 //
-//  Copyright (c) 2013 Push IO LLC. All rights reserved.
+//  Created by Kendall Helmstetter Gelner on 9/17/12.
+//  Copyright (c) 2012 Push IO LLC. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
