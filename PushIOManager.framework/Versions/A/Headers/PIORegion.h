25b6ad4a354cdce0f076df846dfd374a86bd85b3
Cindy Hsu <cindy.hsu@udemy.com>
2015-09-15 15:56:19 -0700

    Update to version 2.13

--- PushIOManager.framework/Versions/A/Headers/PIORegion.h
+++ PushIOManager.framework/Versions/A/Headers/PIORegion.h
@@ -9,21 +9,35 @@
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
 
+/**
+ @discussion This class wraps a CLRegion, adding extra metadata used by PushIO.
+ */
+
 @interface PIORegion : NSObject
 
-// Holds radius, lat/long, and identifier
+/**
+ Holds radius, lat/long, and identifier
+ */
 @property (nonatomic, strong) CLRegion *systemRegion;
 
-// Repeat location for quicker sorting.
-@property (nonatomic, strong) CLLocation *centerLocation;
+/**
+Repeat location for quicker sorting.
+*/
+ @property (nonatomic, strong) CLLocation *centerLocation;
 
-// Repeat ID for quick access and easier sorting.
+/**
+Repeat ID for quick access and easier sorting.
+*/
 @property (nonatomic, strong) NSString *identifier;
 
-// The system will maintain a count of times a region has been entered.
+/**
+ The system will maintain a count of times a region has been entered.
+ */
 @property (nonatomic, assign) NSInteger regionEntryCount;
 
-// Optional - present only if regions are loaded from remote layers (defined on PushIO servers)
+/**
+ Optional - present only if regions are loaded from remote layers (defined on PushIO servers)
+ */
 @property (nonatomic, strong) NSString *layerID;
 
 @end
